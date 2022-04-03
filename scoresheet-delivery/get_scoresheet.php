8903909f59b2a92bd1dcce618bdcb5981df4c909
Gene Wood <gene_wood@cementhorizon.com>
2022-04-03 14:38:37 -0700

    Add prefix and 403 to get_scoresheet

--- scoresheet-delivery/get_scoresheet.php
+++ scoresheet-delivery/get_scoresheet.php
@@ -5,7 +5,7 @@ $prefix = "My-Homebrew-Competition-Name-2018-Scoresheet-";
 $scoresheet_path = "/path/to/scoresheets/";
 
 if (isset($_GET['id']) and isset($_GET['hash']) and hash("sha256", $_GET['id'].$salt) == $_GET['hash']) {
-  $scoresheetfilename = $scoresheet_path.str_pad($_GET['id'], 4, "0", STR_PAD_LEFT).".pdf";
+  $scoresheetfilename = $scoresheet_path.$prefix.str_pad($_GET['id'], 4, "0", STR_PAD_LEFT).".pdf";
   header('Content-type: application/pdf');
   header('Content-Disposition: attachment; filename="' . $prefix . $_GET['id'] . '.pdf"');
   header('Content-Transfer-Encoding: binary');
@@ -14,5 +14,8 @@ if (isset($_GET['id']) and isset($_GET['hash']) and hash("sha256", $_GET['id'].$
   ob_clean();
   flush();
   readfile($scoresheetfilename);
+} else {
+  header('HTTP/1.0 403 Forbidden');
+  echo 'Sorry, that link appears to be bad. Please <a href="https://www.worldcupofbeer.com/contact/">contact the organizer</a>.';
 }
 ?>
